5b2b91fc60b3bdb109091f777561d1ec543cdf66
Baivo#0110 <braydennepean@gmail.com>
2023-05-23 14:14:17 +1000

    Create TeamsWebhook.py

---
+++ General Purpose/TeamsWebhook.py
@@ -0,0 +1,36 @@
+import requests
+import json
+import os
+
+def send_teams_message(title, body):
+    # This is needed to get around self signed cert SSL errors. You may need to provide your own.
+    cert_path = os.path.join(os.getcwd(), 'cert.pem')
+    os.environ["REQUESTS_CA_BUNDLE"] = cert_path
+    os.environ["SSL_CERT_FILE"] = cert_path
+
+    # Teams webhook URL
+    #webhook_url = "https://outlook.office.com/webhook/..."  
+
+    # Message card
+    message_card = {
+        "@context": "https://schema.org/extensions",
+        "@type": "MessageCard",
+        "title": title,
+        "text": "\n".join(body)
+    }
+
+    # Send the message card
+    response = requests.post(
+        webhook_url,
+        headers={'Content-Type': 'application/json'},
+        data=json.dumps(message_card)
+    )
+
+    # Print the response
+    if response.status_code == 200:
+        print("Message sent successfully!")
+    else:
+        print(f"Failed to send message: {response.status_code}, {response.text}")
+
+# Call the function
+send_teams_message("Hello, World!", ["This is the first line of the body.", "This is the second line of the body."])
